1f03d078e489e6715cae35d3f28de9f16ba9e979
SHIKHAR SINGH <shikhars.singh27@gmail.com>
2025-03-28 00:33:53 +0530

    .

---
+++ leetCODE/strings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter your name:");
+        // String name = sc.nextLine();
+        // System.out.println("your name is:" + name);
+        String firstname = "Shikhar";
+        String lastname = " Singh";
+        String fullname = firstname + lastname;
+        for (int i = 0; i < fullname.length(); i++) {
+            System.out.println(fullname.charAt(i));
+        }
+    }
+}
