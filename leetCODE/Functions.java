eb2fc6f39c4869070a74c0b87b1446c90efcca3d
SHIKHAR SINGH <shikhars.singh27@gmail.com>
2025-04-23 13:47:49 +0530

    .

--- leetCODE/Functions.java
+++ leetCODE/Functions.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Functions {
-    public static void printName(String name) {
-        System.out.println(name);
-        return;
+    public static int PRod(int num1, int num2) {
+        return num1 * num2;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        printName(name);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(PRod(num1, num2));
     }
 }
