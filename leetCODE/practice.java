cf82416cf2b5c176e37ac48e7e58cd356612d027
SHIKHAR SINGH <shikhars.singh27@gmail.com>
2025-07-22 01:42:39 +0530

    .

--- leetCODE/practice.java
+++ leetCODE/practice.java
@@ -1,7 +1,28 @@
+import java.util.*;
+
 public class practice {
     public static void main(String[] args) {
-        // System.out.println("Hello World
-        System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age:");
+        int age = sc.nextInt();
+
+        System.out.print("what is your gpa?");
+        double gpa = sc.nextDouble();
+
+        System.out.print("are you a student? (true/false): ");
+        boolean isStudent = sc.nextBoolean();
+
+        System.out.println("hello " + name);
+        System.out.println("your age: " + age);
+        System.out.println("your gpa is: " + gpa);
+        // System.out.println("Student: " + isStudent);
+
+        sc.close();
+
     }
-  }
-  
+}
